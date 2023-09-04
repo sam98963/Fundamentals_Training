b24db9063bed8ffe5e06cc99ce95288d60896436
sam98963 <szwilliamson01@gmail.com>
2023-09-04 13:50:04 +0100

    build towers

--- Kata_069.js
+++ Kata_069.js
@@ -1,12 +1,10 @@
 function towerBuilder(nFloors) {
   let resultArray = []
-  let block = "*"
-  let gap = " "
   let maxWidth = (nFloors*2)-1
   for(let i = 1; i<= nFloors; i++){
   let blockCount = (2 * i)-1
   let gapAmount = Math.floor((maxWidth - blockCount)/2)
-  let string = gap.repeat(gapAmount) + block.repeat(blockCount) + gap.repeat(gapAmount)
+  let string = " ".repeat(gapAmount) + "*".repeat(blockCount) + " ".repeat(gapAmount)
   resultArray.push(string)
   }
 return resultArray
